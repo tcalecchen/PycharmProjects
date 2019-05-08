339354bcd4c735a023a6485e13ff6429e5d5d76a
alec chen <alec.h.j.chen@gmail.com>
2019-05-08 17:40:49 +0800

    update

---
+++ alec/mypython/turtle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import turtle
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+turtle.done()
